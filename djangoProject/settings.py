078e63df435af958642fe0eb5456b1421cc96857
NEKETA <molnei1996@gmail.com>
2024-04-11 19:56:16 +0300

    2

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -122,3 +122,28 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+     'version': 1,
+     'disable_existing_loggers': False,
+     'handlers': {
+         'console': {
+            'class': 'logging.StreamHandler',
+         },
+         'file': {
+            'class': 'logging.FileHandler',
+            'filename': './log/django.log',
+         },
+     },
+     'loggers': {
+         'django': {
+             'handlers': ['console', 'file'],
+             'level': 'INFO',
+         },
+         'myapp': {
+             'handlers': ['console'],
+             'level': 'DEBUG',
+             'propagate': True,
+         },
+     },
+}
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+import logging
 from django.http import HttpResponse
 
-
+logger = logging.getLogger(__name__)
 def index(request):
-    return HttpResponse("Главная")
+    return HttpResponse("Добро пожаловать на мой первый Django-сайт!")
 
 
 def about(request):
-    return HttpResponse("Привет! Меня зовут [ваше имя] и я начинающий веб-разработчик. Я увлекаюсь созданием веб-сайтов и разработкой приложений. Сейчас я изучаю Django и очень рад поделиться с вами результатами своего обучения.")
+    return HttpResponse("Привет! Я начинающий веб-разработчик. Я увлекаюсь созданием веб-сайтов и разработкой приложений. Сейчас я изучаю Django и очень рад поделиться с вами результатами своего обучения.")
 # Create your views here.
